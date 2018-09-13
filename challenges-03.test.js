d5141ebe6ad0a44a1adc3652e1709bed860537f5
dhwang21 <hwang.derrick@gmail.com>
2018-09-12 19:35:56 -0700

    finished challenge 3

--- challenges-03.test.js
+++ challenges-03.test.js
@@ -76,10 +76,21 @@ const salesData = (hours, data) => {
 // ------------------------------------------------------------------------------------------------
 
 const giveValentines = (list) => {
-  // Solution code here...
+
+const valentinesMessage = [];
+
+list.forEach((messageGiven) => {
+  list.forEach((messageReceived) => {
+    if (messageReceived !== messageGiven) {
+      valentinesMessage.push(`${messageGiven} gives a Valentine to ${messageReceived}.`);
+    }
+});
+});
+
+return valentinesMessage;
 };
 
-// ------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
 // CHALLENGE 4
 //
 // Write a function named findFourteen that returns the number 14 from the nested array.
@@ -242,53 +253,53 @@ const excel = (str) => {
 //   });
 // });
 
-describe('Testing challenge 2', () => {
-  test('It should create an object of data for each store', () => {
-    expect(salesData(hoursOpen, grandTotal(cookieStores))).toStrictEqual([
-      { sales: '88 cookies', time: '9 a.m.' },
-      { sales: '153 cookies', time: '10 a.m.' },
-      { sales: '252 cookies', time: '11 a.m.' },
-      { sales: '286 cookies', time: '12 p.m.' },
-      { sales: '139 cookies', time: '1 p.m.' },
-      { sales: '161 cookies', time: '2 p.m.' },
-      { sales: '145 cookies', time: '3 p.m.' },
-      { sales: '232 cookies', time: '4 p.m.' },
-      { sales: '276 cookies', time: '5 p.m.' },
-      { sales: '207 cookies', time: '6 p.m.' },
-      { sales: '161 cookies', time: '7 p.m.' },
-      { sales: '169 cookies', time: '8 p.m.' }
-    ]);
-
-    expect(salesData(hoursOpen, grandTotal(cookieStores)).length).toStrictEqual(hoursOpen.length);
-  });
-});
-
-// describe('Testing challenge 3', () => {
-//   test('It should return a list of valentine exchanges', () => {
-//     expect(giveValentines(['Jerry', 'George', 'Elaine', 'Kramer', 'Newman'])).toStrictEqual([
-//       'Jerry gives a Valentine to George.',
-//       'Jerry gives a Valentine to Elaine.',
-//       'Jerry gives a Valentine to Kramer.',
-//       'Jerry gives a Valentine to Newman.',
-//       'George gives a Valentine to Jerry.',
-//       'George gives a Valentine to Elaine.',
-//       'George gives a Valentine to Kramer.',
-//       'George gives a Valentine to Newman.',
-//       'Elaine gives a Valentine to Jerry.',
-//       'Elaine gives a Valentine to George.',
-//       'Elaine gives a Valentine to Kramer.',
-//       'Elaine gives a Valentine to Newman.',
-//       'Kramer gives a Valentine to Jerry.',
-//       'Kramer gives a Valentine to George.',
-//       'Kramer gives a Valentine to Elaine.',
-//       'Kramer gives a Valentine to Newman.',
-//       'Newman gives a Valentine to Jerry.',
-//       'Newman gives a Valentine to George.',
-//       'Newman gives a Valentine to Elaine.',
-//       'Newman gives a Valentine to Kramer.'
+// describe('Testing challenge 2', () => {
+//   test('It should create an object of data for each store', () => {
+//     expect(salesData(hoursOpen, grandTotal(cookieStores))).toStrictEqual([
+//       { sales: '88 cookies', time: '9 a.m.' },
+//       { sales: '153 cookies', time: '10 a.m.' },
+//       { sales: '252 cookies', time: '11 a.m.' },
+//       { sales: '286 cookies', time: '12 p.m.' },
+//       { sales: '139 cookies', time: '1 p.m.' },
+//       { sales: '161 cookies', time: '2 p.m.' },
+//       { sales: '145 cookies', time: '3 p.m.' },
+//       { sales: '232 cookies', time: '4 p.m.' },
+//       { sales: '276 cookies', time: '5 p.m.' },
+//       { sales: '207 cookies', time: '6 p.m.' },
+//       { sales: '161 cookies', time: '7 p.m.' },
+//       { sales: '169 cookies', time: '8 p.m.' }
 //     ]);
+
+//     expect(salesData(hoursOpen, grandTotal(cookieStores)).length).toStrictEqual(hoursOpen.length);
 //   });
 // });
+
+describe('Testing challenge 3', () => {
+  test('It should return a list of valentine exchanges', () => {
+    expect(giveValentines(['Jerry', 'George', 'Elaine', 'Kramer', 'Newman'])).toStrictEqual([
+      'Jerry gives a Valentine to George.',
+      'Jerry gives a Valentine to Elaine.',
+      'Jerry gives a Valentine to Kramer.',
+      'Jerry gives a Valentine to Newman.',
+      'George gives a Valentine to Jerry.',
+      'George gives a Valentine to Elaine.',
+      'George gives a Valentine to Kramer.',
+      'George gives a Valentine to Newman.',
+      'Elaine gives a Valentine to Jerry.',
+      'Elaine gives a Valentine to George.',
+      'Elaine gives a Valentine to Kramer.',
+      'Elaine gives a Valentine to Newman.',
+      'Kramer gives a Valentine to Jerry.',
+      'Kramer gives a Valentine to George.',
+      'Kramer gives a Valentine to Elaine.',
+      'Kramer gives a Valentine to Newman.',
+      'Newman gives a Valentine to Jerry.',
+      'Newman gives a Valentine to George.',
+      'Newman gives a Valentine to Elaine.',
+      'Newman gives a Valentine to Kramer.'
+    ]);
+  });
+});
 
 // describe('Testing challenge 4', () => {
 //   test('It should return the number 14', () => {
